f6fd69a5a34a1141355e5e0bb4dd0d1425dd5f98
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-10-21 16:12:19 +0600

    Main containers and components are created. Fetch and Promise.all are used to get 5 jokes and display them on screen.

---
+++ src/components/Joke/Joke.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Joke = props => {
+    return (
+        <p>
+            {props.value}
+        </p>
+    );
+};
+
+export default Joke;
---
+++ src/containers/JokesCollector/JokesCollector.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import './JokesCollector.css';
+import Joke from "../../components/Joke/Joke";
+
+class JokesCollector extends Component {
+    state = {
+        jokes: []
+    };
+
+    getJokes = (URL, number) => {
+        const fetchResponsePromises = [];
+        for (let i = 0; i < number; i++) {
+            let fetchResponsePromise = fetch(URL).then(joke => joke.json());
+            fetchResponsePromises.push(fetchResponsePromise);
+        }
+        return fetchResponsePromises;
+    };
+    componentDidMount() {
+        const JOKES_URL = 'https://api.chucknorris.io/jokes/random';
+        const promises = this.getJokes(JOKES_URL, 5);
+        Promise.all(promises)
+            .then(response => {
+                return response.map(joke => {
+                    return joke.value;
+                })
+            }).then(jokes => {
+                this.setState({jokes});
+        });
+    }
+
+    render() {
+        let jokesList = null;
+        if (this.state.jokes.length === 0) {
+            jokesList = (
+                <form>
+                    <p>No jokes about Chuck Norris</p>
+                </form>
+            );
+        } else {
+            jokesList = this.state.jokes.map(joke => (
+                <Joke
+                    value={joke}
+                />
+            ));
+        }
+        return (
+            <div>
+                {jokesList}
+            </div>
+        );
+    }
+}
+
+export default JokesCollector;
